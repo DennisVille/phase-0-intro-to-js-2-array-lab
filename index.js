6e3ef328be867595e0cb92c173510db18876c5c9
dennis <denismukuria@gmail.com>
2024-12-18 17:55:49 +0300

    exercise array methods

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    let cats2 = cats.toSpliced(3,0,name);
+    return cats2;
+}
+function prependCat(name){
+    let cats3 = cats.toSpliced(0,0,name);
+    return cats3;
+}
+function removeLastCat(){
+    let cats4 = cats.slice(0,2);
+    return cats4;
+}
+function removeFirstCat(){
+    let cats5 = cats.slice(1);
+    return cats5;
+}
